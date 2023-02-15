4a8b4d07880f09cd5727df4b3a5cb148e6786b74
wawatai <wataiPublic@gmail.com>
2023-02-15 18:29:23 +0800

    112-02-15*
    recommit

--- js/js.js
+++ js/js.js
@@ -29,12 +29,32 @@ $(function(){
         .removeClass("display");
     })
 
-    $(".rwd_TOPNav").click(function(){
-        $(this)
-        .children().toggleClass("display");
+    $("header .rwd_TOPNav .itemBox:last-child").on("click",function(){
 
         $("main .center .left")
         .toggleClass("active");
+    })
+
+    $(document).on("click",".itemBox.nav",function(){
+        $(this)
+        .addClass("active")
+        .find(".injected-svg")
+        .css("display","block")
+        .siblings("svg")
+        .css("display","none")
+        .siblings("p")
+        .text("閉じる");
+    })
+
+    $(document).on("click",".itemBox.nav.active",function(){
+        $(this)
+        .removeClass("active")
+        .find(".hamIC")
+        .css("display","block")
+        .siblings("svg")
+        .css("display","none")
+        .siblings("p")
+        .text("メニュー");
     })
 })
 
@@ -103,19 +123,67 @@ $(function(){
             prevEl: '.swiper-button-prev02',
           },
     });
+
+    var swiper03 = new Swiper('.swiper03', {
+        autoHeight: true,
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        freeMode: true,
+        navigation: {
+            nextEl: '.swiper-button-next03',
+            prevEl: '.swiper-button-prev03',
+          },
+    });
  
-    //gameTypeBox
-    $(".gameTypeBox").click(function(){
+    //countryList 國別一覽
+    $(".mainArea .countryList .cTitle").click(function(){
+        $(this).parent()
+        .toggleClass("active");
+
+        $(".mainArea .countryList ul")
+        .addClass("active");
+    })
+    
+    //ul smallTitle
+    $(".mainArea .countryList ul .smallTitle").click(function(event){
+        $(this).parent()
+        .toggleClass("active")
+        event.stopPropagation();
+    })
+    $(document).on("click",".fa-regular.fa-star",function(){
+        $(this)
+        .attr("class","fa-solid fa-star");
+    })
+    $(document).on("click",".fa-solid.fa-star",function(){
+        $(this)
+        .attr("class","fa-regular fa-star");
+    })
+ 
+    //topnav
+    $(".topnav li").click(function(){
         $(this)
         .addClass("active")
-        .closest(".swiper-slide")
+        .siblings()
+        .removeClass("active");
+    })
+ 
+    //gameTypeBox
+    $(".typselect .gameTypeBox").click(function(){
+        $(this)
+        .toggleClass("active")
+        .closet("swiper-slide")
         .siblings()
         .find(".gameTypeBox")
         .removeClass("active");
     })
 
+    $(".betRecordBtn").click(function(){
+        $("main .center .right")
+        .addClass("active");
+    })
+
     //gBlock 投注
-    $(".pkTeam .team button").click(function(){
+    $(".center .gBlock .down .pkTeam .team button").click(function(){
         $(this)
         .addClass("active")
         .closest(".team")
@@ -175,25 +243,6 @@ $(function(){
             .css("height",""+ (wh - 90) +"");
         })
     }
-
-    $(window).resize(function(){
-        var ww = $(window).innerWidth();
-
-        if(ww < 1024)
-        {
-            var wh = $(window).innerHeight();
-
-            $(".left")
-            .css("height",""+ (wh - 90) +"");
-
-            $(window).resize(function(){
-                var wh = $(window).innerHeight();
-
-                $(".left")
-                .css("height",""+ (wh - 90) +"");
-            })
-        }
-    })
 })
 
 //gameDetail
